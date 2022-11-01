1d3f2b611adfa1495cf37942a6afb2f7d9b41646
reuseman <alexcolucci@protonmail.com>
2022-11-01 10:34:35 +0100

    Add Ast with helpers

--- mcomp-codegen/lib/ast.ml
+++ mcomp-codegen/lib/ast.ml
@@ -1,15 +1,18 @@
 type binop =
+  (* Math operands *)
   | Add
   | Sub
   | Mult
   | Div
   | Mod
+  (* Compare operands *)
   | Equal
   | Neq
   | Less
   | Leq
   | Greater
   | Geq
+  (* Bool operands *)
   | And
   | Or
 [@@deriving show, ord, eq]
@@ -19,15 +22,15 @@ type uop = Neg | Not [@@deriving show, ord, eq]
 type identifier = string [@@deriving show, ord, eq]
 
 type typ =
-  | TInt (* Type int *)
-  | TBool (* Type bool *)
-  | TChar (* Type char *)
-  | TArray of typ * int option (* Array type *)
-  | TRef of typ (* Reference type *)
-  | TVoid (* Type unit *)
-  | TFun of typ list * typ (* Type functions [paramerts] -> return_type *)
-  | TInterface of identifier  (* Type of an interface *)
-  | TComponent of identifier (* Type of a component *)
+  | TInt                        (* Type int *)
+  | TBool                       (* Type bool *)
+  | TChar                       (* Type char *)
+  | TArray of typ * int option  (* Array type *)
+  | TRef of typ                 (* Reference type *)
+  | TVoid                       (* Type unit *)
+  | TFun of typ list * typ      (* Type functions [paramerts] -> return_type *)
+  | TInterface of identifier    (* Type of an interface *)
+  | TComponent of identifier    (* Type of a component *)
 [@@deriving show, ord, eq]
 
 type ('a, 'b) annotated_node = { node : 'a; annot : 'b }
@@ -38,49 +41,49 @@ type vdecl = identifier * typ [@@deriving show, ord, eq]
 type 'a expr = ('a expr_node, 'a) annotated_node
 
 and 'a expr_node =
-  | LV of 'a lvalue (* x or a[e] *)
-  | Assign of 'a lvalue * 'a expr (* x=e or a[e]=e *)
-  | ILiteral of int (* Integer literal *)
-  | CLiteral of char (* Char literal *)
-  | BLiteral of bool (* Bool literal *)
-  | UnaryOp of uop * 'a expr (* Unary primitive operator *)
-  | Address of 'a lvalue (* Address of a variable *)
-  | BinaryOp of binop * 'a expr * 'a expr (* Binary primitive operator *)
-  | Call of identifier option * identifier * 'a expr list
-(* Function call f(...) *)
+  | LV of 'a lvalue                                       (* x or a[e] *)
+  | Assign of 'a lvalue * 'a expr                         (* x=e or a[e]=e *)
+  | ILiteral of int                                       (* Integer literal *)
+  | CLiteral of char                                      (* Char literal *)
+  | BLiteral of bool                                      (* Bool literal *)
+  | UnaryOp of uop * 'a expr                              (* Unary primitive operator *)
+  | Address of 'a lvalue                                  (* Address of a variable *)
+  | BinaryOp of binop * 'a expr * 'a expr                 (* Binary primitive operator *)
+  | Call of identifier option * identifier * 'a expr list (* Function call f(...) *)
 [@@deriving show, ord, eq]
 
 and 'a lvalue = ('a lvalue_node, 'a) annotated_node
 
 and 'a lvalue_node =
-  | AccVar of identifier option * identifier (* Variable access x *)
-  | AccIndex of 'a lvalue * 'a expr
-(* Array indexing a[e] *)
+  | AccVar of identifier option * identifier  (* Variable access x, where the first identifier is an optional qualifier *)
+  | AccIndex of 'a lvalue * 'a expr           (* Array indexing a[e] *)
 [@@deriving show, ord, eq]
 
 and 'a stmt = ('a stmt_node, 'a) annotated_node
 
 and 'a stmt_node =
-  | If of 'a expr * 'a stmt * 'a stmt (* Conditional *)
-  | While of 'a expr * 'a stmt (* While loop *)
-  | Expr of 'a expr (* Expression statement e; *)
-  | Return of 'a expr option (* Return statement *)
-  | Block of 'a stmtordec list (* Block: grouping and scope *)
+  | If of 'a expr * 'a stmt * 'a stmt                                 (* Conditional *)
+  | While of 'a expr * 'a stmt                                        (* While loop *)
+  | DoWhile of 'a expr * 'a stmt                                      (* Do-While loop *)
+  | For of 'a expr option * 'a expr option * 'a expr option * 'a stmt (* For loop *)
+  | Expr of 'a expr                                                   (* Expression statement e; *)
+  | Return of 'a expr option                                          (* Return statement *)
+  | Block of 'a stmtordec list                                        (* Block: grouping and scope *)
   | Skip
 [@@deriving show, ord, eq]
 
 and 'a stmtordec = ('a stmtordec_node, 'a) annotated_node
 
 and 'a stmtordec_node =
-  | LocalDecl of vdecl (* Local variable declaration *)
-  | Stmt of 'a stmt (* A statement *)
+  | LocalDecl of vdecl  (* Local variable declaration *)
+  | Stmt of 'a stmt     (* A statement *)
 [@@deriving show, ord, eq]
 
 and 'a fun_decl = {
   rtype : typ;
   fname : identifier;
   formals : vdecl list;
-  body : 'a stmt option; (* None when is a prototype *)
+  body : 'a stmt option; (* None when is a declaration, Some when definition *)
 }
 [@@deriving show, ord, eq]
 
@@ -131,3 +134,93 @@ type located_compilation_unit = Location.code_pos compilation_unit
 
 type typed_compilation_unit = typ compilation_unit
 [@@deriving show, ord, eq]
+
+
+(* Extra type needed to keep track of all the top level declarations for Menhir *)
+type 'a top_decl = 
+  | Interface of 'a interface_decl
+  | Component of 'a component_decl
+  | Connection of connection list
+[@@deriving show, ord, eq]
+
+
+(* --- Helper functions --- *)
+
+
+(**
+  Annotate a node with a given annotation.
+  @param a The node to annotate.
+  @param b The annotation to use.
+  @return The annotated node.
+*)
+let annotate_node a b = {node=a; annot=b}
+
+
+(**
+  Check weather a type is a primitive type (e.g. int, bool, char)
+  @return True if the type is a primitive type, false otherwise.
+*)
+let is_primitive = function 
+| TInt | TBool | TChar -> true
+| _ -> false
+
+(**
+  Check weather a type is a scalar type (e.g. array or array reference)
+  @return True if the type is a scalar type, false otherwise.
+*)
+let rec is_scalar = function
+| TArray(_) -> true
+| TRef(typ) -> is_scalar typ
+| _ -> false
+
+(**
+  Check weather a binary operation is a math operation (e.g. +, -, *, /) 
+  @return True if the binary operation is a math operation, false otherwise.
+*)
+let is_math_op = function
+| Add | Sub | Mult | Div | Mod -> true
+| _ -> false
+
+(**
+  Check weather a binary operation is a comparison operation (e.g. <, >, <=, >=)
+  @return True if the binary operation is a comparison operation, false otherwise.
+*)
+let is_comp_op = function
+| Equal | Neq | Less | Leq | Greater | Geq -> true
+| _ -> false
+
+(**
+  Check weather a binary operation is a logical operation (e.g. &&, ||)
+  @return True if the binary operation is a logical operation, false otherwise.
+*)
+let is_bool_op = function
+| And | Or -> true
+| _ -> false
+
+
+(**
+  Check weather a given typ is equal to a specific type or to the reference over the type.
+  @param oracle_typ The type that will be compared to x.
+  @param to_check The type to check.
+  @return True if the types are equal (e.g. int == int, ...) or if the types are over a reference (e.g. int == ref(int), ...), false otherwise.
+*)
+let is_of_typ_or_reftyp oracle_typ to_check =
+  match to_check with
+  | TRef(ttyp) -> equal_typ oracle_typ ttyp
+  | _ -> equal_typ oracle_typ to_check
+
+(**
+  Redefine the derived show over the type in order to have better human readable output.
+  @return A string representation of the type.
+*)
+let rec show_typ = function
+| TInt -> "int"
+| TBool -> "bool"
+| TChar -> "char"
+| TArray (t, None) -> Printf.sprintf "%s[]" (show_typ t)
+| TArray (t, Some n) -> Printf.sprintf "%s[%d]" (show_typ t) n
+| TRef t -> Printf.sprintf "ref %s" (show_typ t)
+| TVoid -> "void"
+| TFun (args, ret) -> Printf.sprintf "(%s) -> %s" (String.concat ", " (List.map show_typ args)) (show_typ ret)
+| TInterface i -> Printf.sprintf "interface %s" i
+| TComponent c -> Printf.sprintf "component %s" c
