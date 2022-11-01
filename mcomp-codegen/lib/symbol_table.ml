0f3ab22c14f887f41dfd8579541c24ca1c9cf4c8
reuseman <alexcolucci@protonmail.com>
2022-11-01 12:02:37 +0100

    Add symbol table

--- mcomp-codegen/lib/symbol_table.ml
+++ mcomp-codegen/lib/symbol_table.ml
@@ -1,15 +1,51 @@
 exception DuplicateEntry of Ast.identifier
+exception NotFound of Ast.identifier
+
+type 'a t = SymbolTable of (Ast.identifier, 'a) Hashtbl.t list
+
+let empty_table () = SymbolTable( [Hashtbl.create 0] )
+
+let begin_block = function
+| SymbolTable(tables) -> SymbolTable(Hashtbl.create 0 :: tables)
+
+let end_block = function
+| SymbolTable([]) -> raise (Failure "Cannot end block of empty symbol table")
+| SymbolTable(_ :: tables) -> SymbolTable(tables)
 
 
-type 'a t = 'a   (* A Dummy type *)
-let empty_table = failwith "Not, implemented yet"
+let add_entry symbol info = function
+| SymbolTable([]) -> raise (Failure "Cannot add entry to empty symbol table")
+| SymbolTable(table :: xs) -> 
+  if Hashtbl.mem table symbol then raise (DuplicateEntry symbol)
+  else Hashtbl.add table symbol info;
+  SymbolTable(table :: xs)
+  
+let rec lookup symbol = function
+| SymbolTable([]) -> raise (NotFound symbol)
+| SymbolTable(table :: xs) -> 
+  try Hashtbl.find table symbol
+  with Not_found -> lookup symbol (SymbolTable(xs))
 
-let begin_block = failwith "Not, implemented yet"
 
-let end_block = failwith "Not, implemented yet"
+let of_alist list = 
+  let table = empty_table () in
+  List.iter (fun (symbol, info) -> let _ = add_entry symbol info table in ()) list;
+  table
 
-let add_entry = failwith "Not, implemented yet"
+let iter f = function
+| SymbolTable([]) -> raise (Failure "Cannot iterate over empty symbol table")
+| SymbolTable(table :: _) -> Hashtbl.iter f table
 
-let lookup = failwith "Not, implemented yet"
+let fold f init = function
+| SymbolTable([]) -> raise (Failure "Cannot fold over empty symbol table")
+| SymbolTable(table :: _) -> Hashtbl.fold f table init
 
-let of_alist = failwith "Not, implemented yet"
+let show = function
+| SymbolTable([]) -> raise (Failure "Cannot show empty symbol table")
+| SymbolTable(table :: xs) -> 
+  let show_table table = 
+    let keys = Hashtbl.fold (fun key _ acc -> key :: acc) table [] in
+    "[" ^ String.concat ", " keys ^ "]"
+  in
+  let tables = table :: xs in
+  String.concat " | " (List.map show_table tables)
--- mcomp-codegen/lib/symbol_table.mli
+++ mcomp-codegen/lib/symbol_table.mli
@@ -1,8 +1,13 @@
 exception DuplicateEntry of Ast.identifier
+exception NotFound of Ast.identifier
+
 type 'a t 
-val empty_table : 'a t 
+val empty_table : unit -> 'a t 
 val begin_block : 'a t -> 'a t 
 val end_block : 'a t -> 'a t
 val add_entry : Ast.identifier -> 'a -> 'a t -> 'a t 
 val lookup : Ast.identifier -> 'a t -> 'a
 val of_alist : (Ast.identifier * 'a) list -> 'a t 
+val iter : (string -> 'a -> unit) -> 'a t -> unit
+val fold: (string -> 'a -> 'b -> 'b) -> 'b -> 'a t -> 'b
+val show : 'a t -> string
