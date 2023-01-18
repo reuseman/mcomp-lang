101e42351a47a485a662e37f83e78c6c68609100
reuseman <alexcolucci@protonmail.com>
2023-01-18 17:34:40 +0100

    Add comments to linker

--- mcomp-codegen/lib/linker.ml
+++ mcomp-codegen/lib/linker.ml
@@ -5,14 +5,18 @@ open Ast
 (** Infix operator to annotate node with its type *)
 let ( ++ ) a b = annotate_node a b
 
+
 type link_table = (Ast.identifier, Ast.identifier) Hashtbl.t
 
+
+(** The data that is used in the Hashtbl to keep track of the needed information to check and link. *)
 type component_data =
   | ComponentData of {
       uses : Ast.identifier list;
       provides : Ast.identifier list;
       links : link_table;
     }
+
 
 (** Helpers to raise Linking errors with an error message *)
 let build_msg (err_msg : string) (help_msg : string) =
@@ -21,15 +25,40 @@ let build_msg (err_msg : string) (help_msg : string) =
 let linking_error err_msg help_msg =
   raise (LinkingError (build_msg err_msg help_msg))
 
+
+
+(* CHECKER SUB-MODULE*)
+
 module Checker = struct
+
+  (**
+      Given the identifiers that made up a link, return a readable string.
+      @param l_comp The identifier of the left component
+      @param l_inter The identifier of the left interface
+      @param r_comp The identifier of the right component
+      @param r_inter The identifier of the right interface
+      @return A string representing the link    
+  *)
   let show_link l_comp l_inter r_comp r_inter =
     l_comp ^ "." ^ l_inter ^ " <- " ^ r_comp ^ "." ^ r_inter
 
+  (**
+      Given a link, return a readable string.
+      @param link The link to show
+      @return A string representing the link
+  *)
   let show_link link =
     match link with
     | Ast.Link (l_comp, l_inter, r_comp, r_inter) ->
         show_link l_comp l_inter r_comp r_inter
 
+  (**
+      Given a component table, check if an alleged component identifier, is actually defined.
+      @param component_table The table of components
+      @param comp The component to check
+      @param link The link that contains the component
+      @raise LinkingError if the component is not defined
+  *)
   let check_id_is_component component_table comp link =
     try
       let _ = Hashtbl.find component_table comp in
@@ -44,6 +73,13 @@ module Checker = struct
       in
       linking_error err_msg help_msg
 
+  (**
+      Given a component table, check that the left and right components are different.
+      @param l_comp The left component
+      @param r_comp The right component
+      @param link The link that contains the components
+      @raise LinkingError if the components are the same
+  *)
   let check_components_are_different l_comp r_comp link =
     if l_comp = r_comp then
       let err_msg =
@@ -55,6 +91,15 @@ module Checker = struct
       linking_error err_msg help_msg
     else ()
 
+
+  (**
+    Given a component table, check that the alleged interface used by the left component is actually used.
+    @param table The table of components
+    @param component The left component
+    @param interface The alleged interface used by the left component
+    @param link The link that contains the components
+    @raise LinkingError if the interface is not used
+  *)
   let check_component_uses_interface table component interface link =
     let (ComponentData { uses; _ }) = Hashtbl.find table component in
     if List.mem interface uses then ()
@@ -70,6 +115,15 @@ module Checker = struct
       in
       linking_error err_msg help_msg
 
+
+  (**
+    Given a component table, check that the alleged interface provided by the right component is actually provided.
+    @param table The table of components
+    @param component The right component
+    @param interface The alleged interface provided by the right component
+    @param link The link that contains the components
+    @raise LinkingError if the interface is not provided
+  *)
   let check_component_provides_interface table component interface link =
     let (ComponentData { uses = _; provides; _ }) =
       Hashtbl.find table component
@@ -87,6 +141,16 @@ module Checker = struct
       in
       linking_error err_msg help_msg
 
+  (**
+    Given a component table, check that the alleged interfaces match.
+    @param table The table of components
+    @param l_comp The left component
+    @param l_inter The alleged interface used by the left component
+    @param r_comp The right component
+    @param r_inter The alleged interface provided by the right component
+    @param link The link that contains the components
+    @raise LinkingError if the interface is not provided
+  *)
   let check_interfaces_are_equal l_inter r_inter link =
     if l_inter <> r_inter then
       let err_msg =
@@ -98,6 +162,17 @@ module Checker = struct
       linking_error err_msg help_msg
     else ()
 
+
+  (**
+    Given a component table, visit a link and check that is a valid one by checking that all the rules are respected.
+    @param table The table of components
+    @param l_comp The left component
+    @param l_inter The alleged interface used by the left component
+    @param r_comp The right component
+    @param r_inter The alleged interface provided by the right component
+    @param link The link that contains the components
+    @raise LinkingError if the interface is not provided
+  *)
   let visit_link component_table link =
     match link with
     (* Given a connection `ID1.ID2 <- ID3.ID4`, the linker must ensure that: *)
@@ -140,9 +215,16 @@ module Checker = struct
                 "The used interface must be linked only to one component."
               in
               linking_error err_msg help_msg
-        (* Otherwise keep track of the link *)
+        (* Otherwise keep track of the link (e.g. qualification) *)
         | None -> Hashtbl.add links l_inter r_comp)
 
+  (**
+    Given the components table and the component, check that all the interfaces used by the component are provided.
+
+    @param table The table of components
+    @param component The component to visit
+    @raise LinkingError if the interface is not provided
+  *)
   let check_used_interfaces_are_provided components component_table =
     let check component =
       match component.node with
@@ -172,11 +254,15 @@ module Checker = struct
                         in
                         linking_error err_msg help_msg)
                 uses)
-      (* Check all components *)
-    in
+    (* Check all components *)
+    in List.iter check components
 
-    List.iter check components
 
+  (**
+    Given the components, visit them and create a table of components with their list of used and provides, and with a
+    @param components The list of components
+    @return The table of components
+  *)
   let visit_components components =
     let visit_component components_table component =
       match component.node with
@@ -190,12 +276,27 @@ module Checker = struct
     List.iter (visit_component components_table) components;
     components_table
 
+  (**
+    Given the components and the links, visit them and check that they are valid.
+    @param components The list of components
+    @param links The list of links
+    @raise LinkingError if any rule is not respected
+
+  *)
   let visit_links components links =
+    (* Visit first the components to get the hashtable *)
     let components_table = visit_components components in
+    (* Visit the link and check the constraints.
+       Each link will be added to the component's link table *)
     List.iter (visit_link components_table) links;
+
     check_used_interfaces_are_provided components components_table;
     components_table
 end
+
+
+
+(* QUALIFIER SUB-MODULE*)
 
 module Qualifier = struct
   let rec visit_lvalue current_cname table lvalue =
@@ -207,7 +308,6 @@ module Qualifier = struct
         Ast.AccIndex (vl, ve) ++ lvalue.annot
     | Ast.AccVar (Some interface, id) ->
         (* Add qualifier *)
-        print_endline ("Accessing interface: " ^ interface ^ " in the table for identifier " ^id);
         let component =
           if interface = "Prelude" then "Prelude"
           else if interface = current_cname then current_cname
@@ -263,7 +363,6 @@ module Qualifier = struct
           Ast.Call (Some(current_cname), i2, ve)
       | Ast.Call (Some interface, i2, exprs) ->
           (* Add qualifier *)
-          print_endline ("Calling interface: " ^ interface ^ " in the table");
           let component =
             if interface = "Prelude" then "Prelude"
             else if interface = current_cname then current_cname
@@ -359,15 +458,9 @@ let wire_components ast =
         (i, co, cn)
   in
 
-  (* Check the semantic of the connections. *)
+  (* Check the semantic of the connections, and get the component table
+      with the qualifications to apply (e.g. links) *)
   let components_table = Checker.visit_links ct connections in
-
-  (* print table *)
-  print_endline "Components table:";
-  Hashtbl.iter
-    (fun k _ ->
-      print_endline (k ^ " -> " ^ ("string_of_component_data")))
-    components_table;
 
   (* Qualify the VarAcc and function Call by substituting the interface name with a concrete component. *)
   let qualified_components = Qualifier.visit_components components_table ct in
