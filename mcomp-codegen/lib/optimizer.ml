248e72addd64f9506585c9eec608f9b1db2a39d7
reuseman <alexcolucci@protonmail.com>
2023-01-18 17:34:45 +0100

    Add optimizers

--- mcomp-codegen/lib/optimizer.ml
+++ mcomp-codegen/lib/optimizer.ml
@@ -4,9 +4,14 @@ let optimize_module llvm_module =
   (* memory to register promontion *)
   Llvm_scalar_opts.add_memory_to_register_promotion pass_manager;
 
+  (* This pass re-associates expressions to make them more amenable to other optimization passes. *)
+  Llvm_scalar_opts.add_reassociation pass_manager;
+
   (* constant propagation *)
-  (* TODO: why doesn't work with this uncommented? *)
   (* Llvm_scalar_opts.add_constant_propagation pass_manager; *)
+
+  (* remove any store to memory that are not used by any later instruction *)
+  Llvm_scalar_opts.add_dead_store_elimination pass_manager;
 
   (* loop unrolling *)
   Llvm_scalar_opts.add_loop_unroll pass_manager;
